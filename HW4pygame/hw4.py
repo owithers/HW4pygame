fe5ee23912d9aff0c55dfe6b1905e95ee384ce01
Olivia Withers <oliviawithers@Olivias-MacBook-Pro-3.local>
2016-12-03 12:49:31 -0500

    final game

--- HW4pygame/hw4.py
+++ HW4pygame/hw4.py
@@ -6,7 +6,7 @@ height = 600
 size = (width, height)
 
 black = (0,0,0)
-white = (255, 255, 255)
+yellow = (255, 255, 0)
 
 pygame.init()
 pygame.mixer.init() 
@@ -16,10 +16,26 @@ pygame.display.set_caption("Emoji Fight")
 shooting_sound = pygame.mixer.Sound("laser.wav")
 gameover = pygame.mixer.Sound("gameover.wav")
 explosion = pygame.mixer.Sound("explosion.wav")
-pygame.mixer.music.set_volume(1) 
 
-font_name = pygame.font.match_font('Arial')
+default_font = pygame.font.get_default_font()
 
+class Alien(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = self.image = pygame.image.load("alien.bmp").convert_alpha()
+        self.rect = self.image.get_rect() 
+        self.radius = int(self.rect.width*.9/2)
+        self.speedy = random.randrange(3, 10) 
+        self.rect.x = random.randrange(0, width - self.rect.width) 
+        self.rect.y = random.randrange(-100, -40)
+         
+
+    def update(self):
+        self.rect.y += self.speedy 
+        if self.rect.top > height + 10:
+            self.rect.x = random.randrange(0, width - self.rect.width) 
+            self.rect.y = random.randrange(-100, -40)
+            self.speedy = random.randrange(3, 10)
 
 class Cat(pygame.sprite.Sprite):
     def __init__(self): 
@@ -29,51 +45,32 @@ class Cat(pygame.sprite.Sprite):
         self.radius = 30 
         self.rect.centerx = width/2
         self.rect.bottom = height-10 
-        self.shooting_wait = 250
+        self.shooting_time = 200
         self.last_shot = pygame.time.get_ticks() 
 
     def update(self):
         self.speedx = 0
         keys = pygame.key.get_pressed()
         if keys[pygame.K_LEFT]:
-            self.speedx = -5 #change to speed up
+            self.speedx = -5 
         if keys[pygame.K_RIGHT]:
-            self.speedx = 5 #change to speed up
+            self.speedx = 5 
         if keys[pygame.K_SPACE]:
             self.shoot()
         self.rect.x += self.speedx
-        if self.rect.right > width: #creating a wall so that our right coord. does not get bigger than width
+        if self.rect.right > width: 
             self.rect.right = width
-        if self.rect.left < 0: #coordinate-based screen
-            self.rect.left = 0 #constraining player movement to screen
-
+        if self.rect.left < 0: 
+            self.rect.left = 0 
     def shoot(self):
-        current = pygame.time.get_ticks()
-        if current - self.last_shot > self.shooting_wait: #speeding up shooting delay time
-            self.last_shot = current
-            laser = Laser(self.rect.centerx, self.rect.top) #bottom of bullet at top of the player
-            everything.add(laser) #add to group
+        time = pygame.time.get_ticks()
+        if time - self.last_shot > self.shooting_time: 
+            self.last_shot = time
+            laser = Laser(self.rect.centerx, self.rect.top) 
+            everything.add(laser) 
             lasers.add(laser)
             shooting_sound.play() 
             shooting_sound.set_volume(.8) 
-
-class Alien(pygame.sprite.Sprite):
-    def __init__(self):
-        pygame.sprite.Sprite.__init__(self)
-        self.image = self.image = pygame.image.load("alien.bmp").convert_alpha()
-        self.rect = self.image.get_rect() 
-        self.radius = int(self.rect.width*.9/2)
-        self.rect.x = random.randrange(0, width - self.rect.width) #will alwas appear between left and right
-        self.rect.y = random.randrange(-100, -40)
-        self.speedy = random.randrange(3, 10) #random assignment of speed
-  
-
-    def update(self):
-        self.rect.y += self.speedy 
-        if self.rect.top > height + 10:
-            self.rect.x = random.randrange(0, width - self.rect.width) #will alwas appear between left and right
-            self.rect.y = random.randrange(-100, -40)
-            self.speedy = random.randrange(1, 10)
 
 class Laser(pygame.sprite.Sprite):
     def __init__(self, x, y):
@@ -96,26 +93,27 @@ class Laser(pygame.sprite.Sprite):
         if y <= 0:
             self.kill()
 
-def text(surf, text, size, x, y):
-    font = pygame.font.Font(font_name, size) #created font object
-    text_on = font.render(text, True, white) #surface for writing for pixels, True is set so we can use an anti-aliased font which is cleaner (adds grey pixels)
-    text_rect = text_on.get_rect() #surface for rect
-    text_rect.midtop = (x, y) #positioning score and lives as found from python documentation
-    surf.blit(text_on, text_rect) 
+def text(surface, text, size, x, y):
+    font = pygame.font.Font(default_font, size) 
+    text_s = font.render(text, True, yellow) 
+    text_rect = text_s.get_rect() 
+    text_rect.center = (x, y) 
+    surface.blit(text_s, text_rect) 
+
 
 everything = pygame.sprite.Group()
 aliens = pygame.sprite.Group()
 lasers = pygame.sprite.Group()
 player = Cat()
-everything.add(player) #add any sprite we create so it gets animated and drawn
-for i in range(20):
-    a = Alien() #make a mob
-    everything.add(a) #add it to the new groups
-    aliens.add(a)
+everything.add(player) 
 
-score = 0
+points = 0
 lives = 3
 
+for i in range(20):
+    a = Alien() 
+    everything.add(a) 
+    aliens.add(a)
 
 
 runs = True
@@ -130,13 +128,20 @@ while runs:
     
     hits = pygame.sprite.groupcollide(aliens, lasers, True, True)
     for hit in hits:
-        score = score + 10 
+        points = points + 5 
         a = Alien() 
         aliens.add(a) 
-        
+    if points >= 50:
+        runs = False
+        screen.fill(black)
+        everything.draw(screen)
+        text(screen, "You win!!", 36, width/2, height/2)
+        pygame.display.flip()
+        break
 
     
-    hits = pygame.sprite.spritecollide(player, aliens, True, pygame.sprite.collide_circle) #mobs are now removed when they hit the player
+
+    hits = pygame.sprite.spritecollide(player, aliens, True, pygame.sprite.collide_circle)
     if hits:
         explosion.play()
         explosion.set_volume(.8)
@@ -149,8 +154,8 @@ while runs:
     
     screen.fill(black)
     everything.draw(screen)
-    text(screen, "Lives: " + str(lives), 36, width/2, 10)
-    text(screen, "Score: " + str(score), 36, width/12, 10) 
+    text(screen, "Lives Left: " + str(lives), 28, width/2, 10)
+    text(screen, "Points: " + str(points), 28, width/12, 10) 
     pygame.display.flip() 
 
 while not runs:
@@ -160,7 +165,7 @@ while not runs:
             pygame.quit()
         screen.fill(black)
         everything.draw(screen)
-        text(screen, "Game Over :/ ", 36, width/2, height/2)
+        text(screen, "Game Over", 36, width/2, height/2)
         pygame.display.flip()
 
 pygame.quit()
